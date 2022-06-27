95a89157decb95fd11d72aa959e0ff5bb7a310dd
galindo-hoang <hmhuy191101@gmail.com>
2022-06-28 01:07:08 +0700

    ui

---
+++ fe/grabClone/app/src/main/java/com/example/grabclone/presentation/TripHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.grabclone.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.grabclone.R
+
+class TripHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_history)
+    }
+}
---
+++ fe/grabClone/app/src/main/java/com/example/grabclone/utils/Constant.kt
@@ -0,0 +1,4 @@
+package com.example.grabclone.utils
+
+object Constant {
+}
