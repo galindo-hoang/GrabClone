a8ca7ef7cbf6800bf8d99eca2b8ae86880aed4ad
Your Name <phamtienquan2001@gmail.com>
2022-07-14 22:19:49 +0700

    upate login

--- fe/CallCenter/src/service/LoginService.ts
+++ fe/CallCenter/src/service/LoginService.ts
@@ -2,20 +2,23 @@ import axios from 'axios';
 import {promises} from "dns";
 
 const LOGIN_FORM_API_URL = "http://localhost:8080/login";
-
-class LoginService {
-  async postLoginForm({username, password}: ReqLogin) {
-    const loginElement = {username: username, password: password} as ReqLogin;
-    console.log(loginElement);
-    return await axios.post(LOGIN_FORM_API_URL, loginElement, {
-      headers: {
-
-       /*'Access-Control-Allow-Origin':"*",*/
-        'Content-Type': 'multipart/form-data',
-
-      },
-    })
+const config={
+  headers:{
+    'Content-Type': 'multipart/form-data',
+  },
+  crossOrigin:{
+    'Access-Control-Allow-Origin':'*',
+  },
+  mode:{
+    'mode':'cors'
   }
+}
+class LoginService {
+   postLoginForm({username, password}: ReqLogin) {
+     const loginElement = {username: username, password: password} as ReqLogin;
+     console.log(loginElement);
+     return axios.post(LOGIN_FORM_API_URL, loginElement, config)
+   }
 }
 
 export default new LoginService()
