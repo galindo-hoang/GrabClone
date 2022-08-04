2d62268ae8b58de0a94906b2d7e7c0103030c376
Your Name <phamtienquan2001@gmail.com>
2022-08-04 15:47:18 +0700

    check

---
+++ be/fcm/src/main/java/com/example/fcm/model/dto/InformationBookingRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.fcm.model.dto;
+
+/**
+ * com.example.fcm.model.dto
+ * Created by Admin
+ * Date 8/4/2022 - 3:31 PM
+ * Description: ...
+ */
+public class InformationBookingRequestDTO {
+    private String userName;
+    private String title;
+    private String body;
+}
