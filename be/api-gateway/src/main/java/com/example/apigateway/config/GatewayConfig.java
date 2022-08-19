1e7d4c7312dbe3a7935728049c7fe379c934d4be
Phuc Le <67599931+hoangphuc552001@users.noreply.github.com>
2022-08-19 15:10:28 +0700

    set header cors

--- be/api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ be/api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:3000")
 public class GatewayConfig {
 
     @Autowired
@@ -17,16 +19,24 @@ public class GatewayConfig {
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("sms", r -> r.path("/api/v1/sms/**")
-                        .filters(f -> f.filter(filter))
+                        .filters(f -> f
+                                .setResponseHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+                                .filter(filter))
                         .uri("lb://SMS"))
                 .route("authentication", r -> r.path("/api/v1/users/**")
-                        .filters(f -> f.filter(filter))
+                        .filters(f -> f
+                                .setResponseHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+                                .filter(filter))
                         .uri("lb://AUTHENTICATION"))
                 .route("booking", r -> r.path("/api/v1/booking/**")
-                        .filters(f -> f.filter(filter))
+                        .filters(f -> f
+                                .setResponseHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+                                .filter(filter))
                         .uri("lb://BOOKING"))
                 .route("fcm", r -> r.path("/api/v1/fcm-publish/**")
-                        .filters(f -> f.filter(filter))
+                        .filters(f -> f
+                                .setResponseHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+                                .filter(filter))
                         .uri("lb://FCM"))
                 .build();
 
